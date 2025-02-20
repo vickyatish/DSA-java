d529c89bc25d0db984152c1e8ba292b5fa19699f
Yatish Sekaran <yatish.cs17@bmsce.ac.in>
2025-02-20 13:06:01 -0800

    middleLinkedList.java added

---
+++ src/linkedList/middleLinkedList.java
@@ -0,0 +1,25 @@
+package linkedList;
+
+public class middleLinkedList {
+    public static Node middleLinkedList(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println(middleLinkedList(head).data);
+    }
+}
